a0ef0acc9f469e7401a2c4f9d0aeb65c82486b85
emmacodes19 <berdae@gmail.com>
2019-10-01 17:34:01 -0500

    added some few things

--- js/index.js
+++ js/index.js
@@ -1,10 +1,12 @@
 // Your code goes here
+
+
 //1 
 const mapImage = document.querySelector('.img-content');
 console.log(mapImage);
 
 mapImage.addEventListener('mouseenter', () => {
-    mapImage.style.transform = 'scale(1.2)';
+    mapImage.style.transform = 'scale(3)';
     mapImage.style.transition = 'all 0.3s';
 });
 
@@ -14,10 +16,11 @@ mapImage.addEventListener('mouseleave', () => {
     mapImage.style.transition = 'all 0.3s';
 })
 
-//3 
+//3  with Stop Propagation
 document.querySelectorAll('.nav-link').forEach(element=> {
-    element.addEventListener('click', () => {
+    element.addEventListener('click', (event) => {
         element.style.color = 'blue';
+        event.stopPropagation();
     })
 })
 
@@ -25,14 +28,14 @@ document.querySelectorAll('.nav-link').forEach(element=> {
 //4 
 const newImg = document.querySelector('.intro img');
 newImg.addEventListener('dblclick', () => {
-    newImg.src =" https://images.unsplash.com/photo-1493398961404-f06dd2f81108?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1055&q=80";
+    newImg.src =" https://images.unsplash.com/photo-1547584370-2cc98b8b8dc8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1051&q=80";
 })
 
 
 //5 
 const resizeImg = document.querySelector('.inverse-content .img-content img');
 window.addEventListener('resize', () => {
-    resizeImg.src = 'https://images.unsplash.com/photo-1500917832468-298fa6292e2b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80'
+    resizeImg.src = 'https://images.unsplash.com/photo-1494870363241-b5225be3dada?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80'
 })
 
 //6 
@@ -41,13 +44,15 @@ const body = document.querySelector('body');
 body.addEventListener('keydown',() => {
 body.style.backgroundColor="blue";
 
+
 })
 
 //7 
 
 const navigation = document.querySelector('.main-navigation');
-navigation.addEventListener('contexmenu', (event) => {
+navigation.addEventListener('click', (event) => {
     navigation.style.border = '5px solid orange';
+   
 })
 
 
@@ -55,6 +60,7 @@ navigation.addEventListener('contexmenu', (event) => {
 const footer = document.querySelector('.footer p');
 footer.addEventListener('mousemove',() => {
 footer.style.color ='hotpink';
+alert('See you soon');
 
 })
 
@@ -68,34 +74,89 @@ footer.style.color ='hotpink';
 })
 
 
-//10 
+
+
+//10 with Stop Propagation
 
  document.querySelectorAll('.destination h4').forEach(ele => {
-     ele.addEventListener('mouseout', ()=> {
-         ele.style.fontSize="5rem";
+     ele.addEventListener('mouseout', (event)=> {
+         ele.style.fontSize="8rem";
          ele.style.color="yellow";
+         event.stopPropagation();
+
      })
+
+     
  })
 
+ document.querySelectorAll('.destination  p').forEach(ele => {
+    ele.addEventListener('mouseout', ()=> {
+        ele.style.color="green";
+        
+        
+    })
 
- 11 
+    
+})
+
+
  
- window.addEventListener('scroll', (event) => {
-     alert ('Where are you going ? ')
+
+
+
+
+ //11 
+ 
+ 
+     const lastBoat = document.querySelector('.content-destination img');
+     lastBoat.addEventListener('wheel', () => {
+         lastBoat.style.transform='scale(0.2)'
+     })
+     
     
     
+ //12 
+
+ window.addEventListener('load', (event) => {
+    alert ('Welcome friend');
+   
+   
+})
+
+//13
+
+window.addEventListener('resize', (event)=> {
+    alert ('You are now in a Responsive Layout')
+    
+})
+
+//14
+
+window.addEventListener('scroll', () => {
+   const change =  document.querySelector('.intro');
+   change.style.backgroundColor="purple";
+
+})
+
+//15
+
+window.addEventListener('scroll', () => {
+    const change =  document.querySelector('.text-content');
+    change.style.backgroundColor="red";
+ 
  })
 
  
+ 
 
-//12 
+//16
 navigation.addEventListener('mouseover', (element)=> {
     navigation.style.backgroundColor="green";
     
     
 })
 
-//13 Stop Navs using preventDefault
+//17 Stop Navs using preventDefault---
 
 const navLinks =
 document.querySelectorAll('.nav .nav-link');
